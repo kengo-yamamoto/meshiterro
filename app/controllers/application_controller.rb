88ee237911ca3fea51f54642ed59ec4b936d1253
山本健剛 <yamamotokengo@yamamotokengounoMacBook-Air.local>
2019-07-15 21:57:42 +0900

    [Finish]5-3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
+	before_action :authenticate_user!
 	protected
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.permit(:sign_up,keys: [:name])
 	end
+	Refile.secret_key = 'b82dcd799ba930532c79b8504b861835b7fec9b4884f6c763cb67b48d66e080710e902ff50a583d5df85e580962bfb229128804aae55bc27d9181b7d65aafd2d'
 end
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,3 +1,4 @@
 class PostImage < ApplicationRecord
 	belongs_to :user
+	attachment :image #ここを追加（_idを含めません）
 end
